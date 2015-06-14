0222335180ccaf87ea97fc88cef597513016c5ca
Lesly Brezilien <leslyb@Leslys-MacBook-Pro.local>
2015-06-14 18:34:39 -0400

    hot and cold js

--- js/app.js
+++ js/app.js
@@ -15,3 +15,55 @@ $(document).ready(function(){
 });
 
 
+
+
+
+	var realNumber = 0;
+	var counter = 0;
+
+
+	var newGame = function(){
+		counter = 0;
+		realNumber = Math.floor(Math.random() * 100)+1;
+		document.getElementById("feedback").innerHTML = "Make Your Guess!";
+		document.getElementById("guessList").innerHTML = " ";
+		document.getElementById("count").innerHTML = counter;
+
+
+	};
+    
+	newGame();
+
+
+	var button = document.getElementById("guessButton");
+		
+		button.addEventListener("click", function (){
+		
+
+		var userinput = parseInt(document.getElementById("userGuess").value);
+
+		var message ;
+
+		if ( userinput == realNumber){ message = " Good Job you did it"}
+		else if (Math.abs(userinput - realNumber) > 30){ var message = " You're colder than cold "}
+		else if (Math.abs(userinput - realNumber) <= 30 && Math.abs(userinput - realNumber) > 10 ){  message = " You're getting closer"}
+		else if (Math.abs(userinput - realNumber) <= 10 && Math.abs(userinput - realNumber) > 2 ){ message = " You're hot, now im scared"}
+		else if (Math.abs(userinput - realNumber) <= 2){ message = " If you don't get this by next turn, something wrong with you"}
+		else { message = " skjdksjhdsk "}
+		
+		counter++;
+
+		var li = document.createElement("li");
+		var guesslist = document.createTextNode(userinput);
+		li.appendChild(guesslist);
+		document.getElementById("guessList").appendChild(li);
+		document.getElementById("feedback").innerHTML = message
+		document.getElementById("guessList").appendChild(li);
+		document.getElementById("count").innerHTML = counter;
+
+
+
+	}, false);
+	
+
+
